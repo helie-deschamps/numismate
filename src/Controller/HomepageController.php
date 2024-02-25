610188e0423766dee075f97962329253be27fbc9
Hélie Deschamps <bonjour@helie.me>
2024-02-25 16:49:18 +0100

    Display of coins on homepage for connected users

--- src/Controller/HomepageController.php
+++ src/Controller/HomepageController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\CoinRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -9,10 +10,17 @@ use Symfony\Component\Routing\Attribute\Route;
 class HomepageController extends AbstractController
 {
     #[Route('/', name: 'app_homepage')]
-    public function index(): Response
+    public function index(CoinRepository $coinRepository): Response
     {
+        if(!$this->getUser()){ // If user is not logged in
+            return $this->redirectToRoute('app_login');
+        }
         return $this->render('homepage/index.html.twig', [
             'controller_name' => 'HomepageController',
+            'user' => $this->getUser(),
+            'user_coins_count' => $this->getUser()->getCoinsOwned()->count(),
+            'coins' => $coinRepository->findAll(),
+            'coins_count' => count($coinRepository->findAll()),
         ]);
     }
 }
